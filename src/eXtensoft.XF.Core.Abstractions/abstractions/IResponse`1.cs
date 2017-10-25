452827a82832ecc1bf0441360d68458fc694b430
robert nelson <robertknelson@gmail.com>
2017-10-24 23:07:46 -0400

    abstractions project setup

---
+++ src/eXtensoft.XF.Core.Abstractions/abstractions/IResponse`1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eXtensoft.XF.Core.Abstractions
+{
+    public interface IResponse<T> where T : class, new()
+    {
+    }
+}
