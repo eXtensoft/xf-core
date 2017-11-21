5f375c36cc9a2c739d76d302a81b81bb344b66de
robert nelson <robertknelson@gmail.com>
2017-11-21 18:35:38 -0500

    response factory

---
+++ src/eXtensoft.XF.Data.Abstractions/IResponseFactory.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eXtensoft.XF.Data.Abstractions
+{
+    public interface IResponseFactory
+    {
+        DataResponse<T> Create<T>() where T : class, new();
+    }
+}
